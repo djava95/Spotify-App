8aff3798bcc87f9ec24bdf0d64c8ec9fe95ff999
Davit Javadyan <javadyan1995@gmail.com>
2022-04-17 14:34:09 +0400

    init

--- src/components/Albums/view-albums.js
+++ src/components/Albums/view-albums.js
@@ -13,7 +13,8 @@ export function ViewArtistAlbums({ handleClick }) {
         <section className='albums-main-cont'>
             <div className='albums-header-cont'>
                 <h3 className='artist-name-cont'> {`Albums`}</h3>
-                <button onClick={handleClick} className='close-btn'> &times; </button></div>
+                <button onClick={handleClick} className='close-btn'> &times; </button>
+            </div>
             <div className='albums-cont'>
                 {albums.map(item => <Album key={uuid()} imageUrl={item.images[1].url} name={item.name} releaseYear={item.release_date.substring(0, 4)} />)}
             </div>
