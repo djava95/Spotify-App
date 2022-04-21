66af414d9b3881533ff07d04cf0b835049c4a93d
Davit Javadyan <javadyan1995@gmail.com>
2022-04-21 21:16:32 +0400

    init

--- src/services/actions/actions.js
+++ src/services/actions/actions.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import {ARTIST_SEARCH, LOAD_ARTIST_ALBUMS,  CLEAR_SEARCH_RESULTS } from './action-types';
 
-let token = localStorage.getItem('token');
-
 export const SearchByName = (inputValue) => async (dispatch) => {
   const res = await axios.get(`https://api.spotify.com/v1/search?q=${encodeURIComponent(inputValue)}&type=artist`, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
     }
   });
   const artists = await res.data.artists.items;
@@ -19,7 +17,7 @@ export const SearchByName = (inputValue) => async (dispatch) => {
 export const LoadArtistAlbums = (artistId) => async (dispatch) => {
   const res = await axios.get(`https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album&market=ES&limit=50&offset=0`, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
     }
   });
   const albums = await res.data.items;  
