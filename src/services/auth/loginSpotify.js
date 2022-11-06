0c38bb6f78c48f9c23b73e62b542cfe75c92c565
Davit Javadyan <javadyan1995@gmail.com>
2022-11-06 17:45:53 +0400

    add env base url variables

--- src/services/auth/loginSpotify.js
+++ src/services/auth/loginSpotify.js
@@ -1,5 +1,5 @@
 const client_id = '7d020a9fe91f4e9da397900ca6fb5787';
-let redirect_uri = 'http://localhost:3000';
+let redirect_uri = process.env.REACT_APP_BASE_URL;  // must be registered in Spotify developer dashboard settings 
 let endPoint  =  'https://accounts.spotify.com/authorize';
 let url = `${endPoint}?response_type=token&client_id=${client_id}&redirect_uri=${redirect_uri}`;
 
