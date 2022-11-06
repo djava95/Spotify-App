b128b669b0a347d06749b1b4b82d1ba412d277f6
Davit Javadyan <javadyan1995@gmail.com>
2022-11-06 23:36:13 +0400

    replace static loader with dynamic logo

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,10 @@
+import SpotifyIcon from '../SpotifyIcon/SpotifyIcon'
 import './Loader.scss'
 
 const Loader = () => {
   return (
     <div className='loader-container'>
-      Loading...
+      <SpotifyIcon />
     </div>
   )
 }
